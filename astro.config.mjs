e835936625a9c549fb92bbbecbb719f538ed3397
Sander de Groot <sander@degroot.dev>
2025-05-17 16:12:12 +0200

    Added flux logo in black and white

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Flux Documentation',
+			logo: {
+				light: './src/assets/flux-logo-black.png',
+				dark: './src/assets/flux-logo-white.png',
+				alt: 'Flux Logo'
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/flux-capacitor-io/flux-docs' }],
 			sidebar: [
 				{
