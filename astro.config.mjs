6141758348cb81151f76c9431541225165a43b0d
Sander de Groot <sander@degroot.dev>
2025-06-04 14:45:12 +0200

    feat(hosting): initial setup for deployment to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,39 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Flux Documentation',
-			logo: {
-				light: './src/assets/flux-logo-black.png',
-				dark: './src/assets/flux-logo-white.png',
-				alt: 'Flux Logo'
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/flux-capacitor-io/flux-docs' }],
-			sidebar: [
-				{
-					label: 'About',
-					autogenerate: { directory: 'about' },
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: 'tutorials' },
-				},
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Flux Documentation',
+          logo: {
+              light: './src/assets/flux-logo-black.png',
+              dark: './src/assets/flux-logo-white.png',
+              alt: 'Flux Logo'
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/flux-capacitor-io/flux-docs' }],
+          sidebar: [
+              {
+                  label: 'About',
+                  autogenerate: { directory: 'about' },
+              },
+              {
+                  label: 'Tutorials',
+                  autogenerate: { directory: 'tutorials' },
+              },
+              {
+                  label: 'Guides',
+                  autogenerate: { directory: 'guides' },
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
