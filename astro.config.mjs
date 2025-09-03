500282f61811aebbd33691e546c8fcaa218feca9
Sander de Groot <sander@degroot.dev>
2025-09-03 17:29:25 +0200

    Fixed several build warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,15 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['import', 'global-builtin', 'color-functions']
+        }
+      }
+    }
+  },
   redirects: {
   },
   integrations: [
